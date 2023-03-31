c17c0771208b411cb81d14f7018c5ddd0ce20176
Brian-Momata <brianmomata@gmail.com>
2023-03-31 14:58:52 +0300

    Configure user's show page to list events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  devise_for :users, controllers: {                       
+    sessions: 'users/sessions'                            
+  }  
   get 'events/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,6 +10,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :events
+  resources :users
 
   root "events#index"
 end
