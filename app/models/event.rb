95f33cbe398b6652efd8e967e4108e94f5f37e1c
Brian-Momata <brianmomata@gmail.com>
2023-04-04 10:32:35 +0300

    Add dependent destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :attended_events
+  has_many :attended_events, dependent: :destroy
   has_many :attendees, through: :attended_events
 end
