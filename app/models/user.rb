e7acd4eaab4afd922db21ed734bda8285a8b5717
Brian-Momata <brianmomata@gmail.com>
2023-04-05 11:20:46 +0300

    Fix source on through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :events, foreign_key: "creator_id", dependent: :destroy
-  has_many :attended_events, dependent: :destroy
-  has_many :events, through: :attended_events
+  has_many :attended_events, foreign_key: "attendee_id", dependent: :destroy
+  # helps me retrieve the events where user is an attendee
+  has_many :events_attended, through: :attended_events, source: :event
 end
