091d92bed015f3c25ddebfb85be57d528eb56ee5
Brian-Momata <brianmomata@gmail.com>
2023-04-05 12:02:07 +0300

    Update user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_many :events, foreign_key: "creator_id", dependent: :destroy
   has_many :attended_events, foreign_key: "attendee_id", dependent: :destroy
   # helps me retrieve the events where user is an attendee
-  has_many :events_attended, through: :attended_events, source: :event
+  has_many :events_invited, through: :attended_events, source: :event
 end
