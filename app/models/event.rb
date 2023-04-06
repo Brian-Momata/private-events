a3bacfbcd9813c7fa086da3222682463691419a2
Brian-Momata <brianmomata@gmail.com>
2023-04-06 11:13:43 +0300

    Add scope for future and past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attended_events, dependent: :destroy
   has_many :attendees, through: :attended_events
+
+  scope :past, -> { where("date < ?", DateTime.now)}
+  scope :future, -> { where("date > ?", DateTime.now)}
 end
