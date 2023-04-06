994fe9ab3d62247733c8030f9c4d7a26c28a1cb7
Brian-Momata <brianmomata@gmail.com>
2023-04-06 07:07:51 +0300

    Add #edit and #update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_event, only: [:edit, :update, :show, :destory]
 
   def index
     @events = Event.all
@@ -19,13 +20,30 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+      if @event.update(event_params)
+        flash[:success] = "Event was successfully updated"
+        redirect_to action: 'show'
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+
   def show
-    @event = Event.find(params[:id])
   end
 
   private
   
   def event_params
     params.require(:event).permit(:location, :date)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
